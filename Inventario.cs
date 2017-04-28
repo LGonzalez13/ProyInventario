a7ec6849d9b145620e2beff7ef60bdbf01413797
Luis Fernando <f3r_4ngels@hotmail.com>
2017-04-27 20:16:13 -0500

    Add files via upload

---
+++ Inventario.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyControlInv
+{
+    class Inventario
+    {
+        private Producto[] inventario;
+        private int numProductos;
+        
+        public Inventario()
+        {
+            inventario = new Producto[20];
+            for (int i = 0; i < 20; i++)
+                inventario[i] = null;
+            numProductos = 0;
+        }
+
+        public void agregar(Producto p)
+        {
+            if(numProductos<20)
+                inventario[numProductos++] = p;
+        }
+
+        public Producto buscar(string nombre)
+        {
+            Producto p = inventario[0];
+            for(int i=0;i<numProductos;i++)
+                if(nombre==inventario[i].nombre)
+                {
+                    p = inventario[i];
+                    break;
+                }
+                else
+                {
+                    p = null;
+                }
+            return p;
+            
+                    
+        }
+
+        public void eliminar(string nombre)
+        {
+            for (int i = 0; i < numProductos; i++)
+                if (nombre == inventario[i].nombre)
+                {
+                    for (int j = i + 1; j < numProductos; j++)
+                    {
+                        inventario[i - 1] = inventario[i];
+                    }
+                    inventario[numProductos--] = null;
+
+                    break;
+                }
+        }
+
+        public string reporte()
+        {
+            string reporte = "";
+            for(int i=0;i<numProductos;i++)
+            {
+                reporte += inventario[i].ToString() + Environment.NewLine + Environment.NewLine;
+            }
+            return reporte;
+        }
+
+
+
+        public override string ToString()
+        {
+            return numProductos.ToString();
+        }
+    }
+}
---
+++ Producto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyControlInv
+{
+    class Producto
+    {
+        //codigo, nombre, cantidad, costo
+        private int _codigo;
+        public int codigo { get { return _codigo; } }
+
+        private string _nombre;
+        public string nombre { get { return _nombre; } }
+
+        private int _cantidad;
+        public int cantidad { get { return _cantidad; } }
+
+        private int _costo;
+        public int costo { get { return _costo; } }
+
+        public Producto(int codigo, string nombre, int cantidad, int costo)
+        {
+            _codigo = codigo;
+            _nombre = nombre;
+            _cantidad = cantidad;
+            _costo = costo;
+        }
+
+        public override string ToString()
+        {
+            return codigo.ToString() + Environment.NewLine + nombre + Environment.NewLine + cantidad.ToString() + " pza(s)" + Environment.NewLine + "$" + costo.ToString();
+        }
+    }
+}
